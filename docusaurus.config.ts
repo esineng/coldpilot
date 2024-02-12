23dc6be3a676ca2a44447fe632389184f99ccb60
Ruikang Tao <rico00121@outlook.com>
2024-02-12 21:54:37 +0800

    chore: baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://esineng.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/device-hub-doc/',
 
 
   // GitHub pages deployment config.
